ba43f37b080d40121dadfdc3bde9f659fe364e9a
Rasha Abu Rkab <111677039+RashaAbuRkab@users.noreply.github.com>
2024-12-14 14:17:20 +0200

    Add files via upload

---
+++ tictactoe/tictactoe.py
@@ -0,0 +1,108 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+    return X if x_count <= o_count else O
+
+def actions(board):
+    return {(i,j) for i in range(3) for j in range (3) if board[i][j] == EMPTY}
+
+
+def result(board, action):
+    if board[action[0]][action[1]] is not EMPTY:
+        raise ValueError("Invalid action: Position already taken")
+
+    new_board = [row[:] for row in board]
+    new_board[action[0]][action[1]] = player(board)
+    return new_board
+
+
+def winner(board):
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            return board[0][i]
+
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+
+    return None
+
+
+def terminal(board):
+    return winner(board) is not None or all(cell is not EMPTY for row in board for cell in row)
+
+
+def utility(board):
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
+
+def minimax(board):
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+
+    def max_value(board):
+        if terminal(board):
+            return utility(board)
+        v = -math.inf
+        for action in actions(board):
+            v = max(v, min_value(result(board, action)))
+        return v
+
+    def min_value(board):
+        if terminal(board):
+            return utility(board)
+        v = math.inf
+        for action in actions(board):
+            v = min(v, max_value(result(board, action)))
+        return v
+
+    if current_player == X:
+        best_score = -math.inf
+        best_action = None
+        for action in actions(board):
+            score = min_value(result(board, action))
+            if score > best_score:
+                best_score = score
+                best_action = action
+        return best_action
+    else:
+        best_score = math.inf
+        best_action = None
+        for action in actions(board):
+            score = max_value(result(board, action))
+            if score < best_score:
+                best_score = score
+                best_action = action
+        return best_action
+
+
